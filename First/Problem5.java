d21e96d405c8625b889943d2a64146763b195590
Kyaa-A <asnaripacalna@gmail.com>
2023-04-23 13:07:19 +0800

    init

--- First/Problem5.java
+++ First/Problem5.java
@@ -1,35 +1,41 @@
 import java.util.Scanner;
+
 public class Problem5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // Read number of requirements
+        // Read the number of requirements
         int n = scan.nextInt();
-        // Read relative weight of each requirement
-        int[] weight = new int[n];
+        // Read the weights of the requirements
+        int[] weights = new int[n];
         for (int i = 0; i < n; i++) {
-            weight[i] = scan.nextInt();
+            weights[i] = scan.nextInt();
         }
-        // Read number of proposals
+        // Read the number of proposals
         int p = scan.nextInt();
-        // Read each proposal and calculate score
-        int bestProposal = -1;
-        double bestScore = Double.NEGATIVE_INFINITY;
-        for (int i = 1; i <= p; i++) {
-            String reqs = scan.next();
-            double price = scan.nextDouble();
-            int score = 0;
+        // Read the proposals and calculate their scores
+        double[] scores = new double[p];
+        scan.nextLine(); // consume the newline character
+        for (int i = 0; i < p; i++) {
+            String[] proposal = scan.nextLine().split(" ");
+            String reqs = proposal[0];
+            double price = Double.parseDouble(proposal[1]);
+            double score = 0;
             for (int j = 0; j < n; j++) {
                 if (reqs.charAt(j) == 'O') {
-                    score += weight[j];
+                    score += weights[j];
                 }
             }
-            double normalizedScore = score / price;
-            if (normalizedScore > bestScore) {
-                bestScore = normalizedScore;
-                bestProposal = i;
+            score /= price;
+            scores[i] = score;
+        }
+        // Find the winning proposal
+        int winner = 0;
+        for (int i = 1; i < p; i++) {
+            if (scores[i] > scores[winner]) {
+                winner = i;
             }
         }
         // Output the winning proposal number
-        System.out.println("Proposal " + bestProposal);
+        System.out.println("Proposal " + (winner + 1));
     }
 }
